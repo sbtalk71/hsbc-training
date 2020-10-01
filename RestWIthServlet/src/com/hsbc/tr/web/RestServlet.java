3adccb916d367893e9a20c943eb35b19c7ca1d6d
sbtal <sbtalk@gmail.com>
2020-10-01 15:40:48 +0530

    rest app

---
+++ RestWIthServlet/src/com/hsbc/tr/web/RestServlet.java
@@ -0,0 +1,50 @@
+package com.hsbc.tr.web;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import org.json.JSONObject;
+
+import com.hsbc.tr.web.entity.Emp;
+
+@WebServlet("/api")
+public class RestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String id = request.getParameter("id");
+		try {
+			Emp e = new Emp(Integer.parseInt(id), "Shantanu", "Hyderabad", 45000);
+
+			if (request.getHeader("Accept").equals("application/json")) {
+				response.setContentType("application/json");
+				JSONObject obj = new JSONObject(e);
+				response.getWriter().write(obj.toString());
+				
+			} else if (request.getHeader("Accept").equals("application/xml")) {
+				response.setContentType("application/xml");
+				JAXBContext context = JAXBContext.newInstance(Emp.class);
+				Marshaller marshaller = context.createMarshaller();
+				marshaller.marshal(e, response.getWriter());
+				
+			} else {
+				response.sendError(406);
+			}
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ RestWIthServlet/src/com/hsbc/tr/web/entity/Emp.java
@@ -0,0 +1,57 @@
+package com.hsbc.tr.web.entity;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Emp {
+
+	private int empId;
+	private String name;
+	private String city;
+	private double salary;
+	
+	public Emp() {
+	}
+
+	
+	public Emp(int empId, String name, String city, double salary) {
+		this.empId = empId;
+		this.name = name;
+		this.city = city;
+		this.salary = salary;
+	}
+
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
